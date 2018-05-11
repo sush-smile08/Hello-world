52927228ae3a1a8cbb0afc6dde8cb3c2f93af01d
sushmita <sushmitashikha8@gmail.com>
2018-05-11 17:55:58 +0530

    contact-form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent  {
+  contactMehods = [
+    { id: 1, name: 'Email' }
+    { id: 2, name: 'Email' }
+  ]
+  log(x) {console.log(x); }
 
-  constructor() { }
-
-  ngOnInit() {
+  submit(f){
+    console.log(f);
   }
 
 }
